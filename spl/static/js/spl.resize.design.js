f7ad14f76243a271f61339b119d0f1ab0d4ef0ac
Augusto Roccasalva <augusto@rocctech.com.ar>
2013-01-29 03:34:59 -0300

    Testing scrollbars customizers

--- spl/static/js/spl.resize.design.js
+++ spl/static/js/spl.resize.design.js
@@ -7,14 +7,18 @@
   "use strict"; // jshint ;_;
 
   var resize = function() {
-    var table_avl_width = $(window).width() - $('aside').width(),
-        content_wrapper = $('#content_wrapper'),
-        scroll_wrapper = $('#scroll_wrapper'),
-        content_avl_height = $(window).height() - content_wrapper.position().top;
+    var table_avl_width = $(window).width() - $('aside').width() - 17,
+        //content_wrapper = $('#content_wrapper'),
+        content = $('#content'),
+        //scroll_wrapper = $('#scroll_wrapper'),
+        //content_avl_height = $(window).height() - content_wrapper.position().top;
+        content_avl_height = $(window).height() - content.position().top - 2;
 
-    content_wrapper.width(table_avl_width);
-    content_wrapper.height(content_avl_height);
-    scroll_wrapper.height(content_avl_height);
+    //content_wrapper.width(table_avl_width);
+    //content_wrapper.height(content_avl_height);
+    //scroll_wrapper.height(content_avl_height);
+    content.width(table_avl_width);
+    content.height(content_avl_height);
   };
 
   $(window).resize(resize);
