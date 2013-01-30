3359331e707fc226f6ab63ec78ec7f4a14a93cb1
Augusto Roccasalva <augusto@rocctech.com.ar>
2013-01-30 03:13:52 -0300

    Degrading to native scrollbars

--- spl/static/js/spl.resize.design.js
+++ spl/static/js/spl.resize.design.js
@@ -8,17 +8,13 @@
 
   var resize = function() {
     var table_avl_width = $(window).width() - $('aside').width() - 17,
-        //content_wrapper = $('#content_wrapper'),
-        content = $('#content'),
-        //scroll_wrapper = $('#scroll_wrapper'),
-        //content_avl_height = $(window).height() - content_wrapper.position().top;
-        content_avl_height = $(window).height() - content.position().top - 2;
+        content_wrapper = $('#content_wrapper'),
+        scroll_wrapper = $('#scroll_wrapper'),
+        content_avl_height = $(window).height() - content_wrapper.position().top;
 
-    //content_wrapper.width(table_avl_width);
-    //content_wrapper.height(content_avl_height);
-    //scroll_wrapper.height(content_avl_height);
-    content.width(table_avl_width);
-    content.height(content_avl_height);
+    content_wrapper.width(table_avl_width);
+    content_wrapper.height(content_avl_height);
+    scroll_wrapper.height(content_avl_height);
   };
 
   $(window).resize(resize);
