56351758e1fe0053b4a942dcb2f4c4438cd88361
Augusto Roccasalva <augusto@rocctech.com.ar>
2013-02-05 01:17:51 -0300

    Working on checkboxes inside buttons

--- spl/static/js/spl.checkboxes.js
+++ spl/static/js/spl.checkboxes.js
@@ -11,4 +11,15 @@
     }
   }).filter(':checked').parents('tr').addClass('selected');
 
+  $('#select_all').click(function(e) {
+    $this = $(this);
+    e.stopPropagation();
+    if ($this.is(':checked')) {
+      $('table tr .checkbox input:not(:checked)').click();
+    } else {
+      $('table tr .checkbox input:checked').click();
+    }
+  });
+
 })(window.jQuery);
+// vim:sw=2
