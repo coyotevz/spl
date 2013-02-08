47358c60f7cae217265cd0bb582a91866949a511
Augusto Roccasalva <augusto@rocctech.com.ar>
2013-02-08 02:38:55 -0300

    Prototype for Backbone.pagination

--- spl/static/js/spl.models.js
+++ spl/static/js/spl.models.js
@@ -6,7 +6,7 @@
 
   "use strict"; // jshint ;_;
 
-  var Supplier, Suppliers, SupplierRow, SuppliersList,
+  var Supplier, Suppliers, SupplierRow, SuppliersListView,
       SupplierControls, AppView;
 
   _.templateSettings = {
@@ -21,8 +21,17 @@
     collection: Suppliers
   });
 
+  Backbone.PaginatedCollection = Backbone.Collection.extend({
+    parse: function(resp, options) {
+      this.page = resp.page;
+      this.num_pages = resp.num_pages;
+      this.num_results = resp.num_results;
+      return resp.objects;
+    },
+  });
+
   /* suppliers collection */
-  Suppliers = Backbone.Collection.extend({
+  Suppliers = Backbone.PaginatedCollection.extend({
     model: Supplier,
     url: '/api/suppliers/',
     comparator: function(d) {
@@ -49,11 +58,57 @@
     remove: function() {
     },
     render: function() {
+      this.$el.html(this.template({
+        id: this.model.id,
+        name: this.model.get('name'),
+        email: this.model.get('email'),
+        phone: this.model.get('phone')
+      }));
+      return this;
     }
   });
 
+  /* Suppliers list view */
+  SuppliersListView = Backbone.View.extend({
+    el: $('table tbody'),
+    collection: Suppliers,
+
+    events: {
+    },
+
+    initialize: function() {
+      _.bindAll(this, 'render');
+      this.collection.bind('reset', this.render);
+    },
+
+    addSupplier: function(supplier) {
+      supplier.rowView = new SupplierRow({model: supplier});
+      this.$el.append(supplier.rowView.render().el);
+    },
+
+    render: function(suppliers) {
+      var self = this;
+      console.log(suppliers);
+      suppliers.each(function(supplier) {
+        self.addSupplier(supplier);
+      });
+    }
+
+  });
+
+
+  AppView = Backbone.View.extend({
+    initialize: function() {
+      this.supplier_list = new SuppliersListView;
+    }
+  });
+
+  var appView = new AppView();
+  window.appView = appView;
   window.Supplier = Supplier;
+  window.Suppliers = Suppliers;
   window.SupplierRow = SupplierRow;
+  window.SuppliersListView = SuppliersListView;
 
 })(window.jQuery);
 // vim:sw=2
