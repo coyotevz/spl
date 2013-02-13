d264c20f5486379fb69b4a41e42a81b0c9fad0b9
Augusto Roccasalva <augusto@rocctech.com.ar>
2013-02-13 03:23:51 -0300

    Working on supplier edit screen design

--- spl/static/js/spl.resize.design.js
+++ spl/static/js/spl.resize.design.js
@@ -7,14 +7,20 @@
   "use strict"; // jshint ;_;
 
   var resize = function() {
-    var table_avl_width = $(window).width() - $('aside').width(),
-        content_wrapper = $('#content_wrapper'),
+    var content_wrapper = $('#content_wrapper'),
         scroll_wrapper = $('#scroll_wrapper'),
-        content_avl_height = $(window).height() - content_wrapper.position().top;
+        content_avl_width = $(window).width() - $('aside').width(),
+        content_avl_height = $(window).height() - content_wrapper.position().top,
+        edit_layout = $('.edit-layout', content_wrapper);
 
-    content_wrapper.width(table_avl_width);
+    content_wrapper.width(content_avl_width);
     content_wrapper.height(content_avl_height);
     scroll_wrapper.height(content_avl_height);
+
+    if (edit_layout.length) {
+      $('.edit-left-panel, .edit-right-panel', edit_layout)
+          .outerWidth((edit_layout.width() - 1) / 2);
+    }
   };
 
   $(window).resize(resize);
