6375d9137c9b9640577a79f2af9820797a2e3307
Augusto Roccasalva <augusto@rocctech.com.ar>
2013-02-15 17:07:58 -0300

    Testing backbone-nested

---
+++ spl/static/js/bb-nested.js
@@ -0,0 +1,48 @@
+/*
+ * Backbone-Nested
+ */
+
+(function($) {
+
+  "use strict";
+
+  Backbone.NestedModel = Backbone.Model.extend({
+
+    get: function(attsStrOrPath) {
+    },
+
+    has: function(attr) {
+    },
+
+    set: function(key, value, opts) {
+    },
+
+    clear: function(options) {
+    },
+
+    add: function(options) {
+    },
+
+    remove: function(attrStr, opts) {
+    },
+
+    toJSON: function() {
+    },
+
+    // privated
+    _delayedTrigger: function() {
+    },
+
+    _delayedChange: function(atrStr, newVal) {
+    },
+
+    _runDelayedTriggers: function() {
+    },
+
+    _setAttr: function(newAttrs, attrPath, newValue, opts) {
+    },
+
+
+  });
+
+})(jQuery);
