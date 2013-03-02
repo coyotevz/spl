601e25254a56306ef3e2907652ab867fecbc49a4
Augusto Roccasalva <augusto@rocctech.com.ar>
2013-03-02 01:34:02 -0300

    Listening some dom-events for SupplierRowView

--- spl/static/js/views/supplier_row_view.js
+++ spl/static/js/views/supplier_row_view.js
@@ -1,14 +1,33 @@
 define([
+  'jquery',
   'views/base/view',
   'text!templates/supplier_row.html'
-], function(View, template) {
+], function($, View, template) {
   "use strict";
 
   var SupplierRowView = View.extend({
     
     template: template,
     tagName: 'tr',
-    className: 'supplier-row'
+    className: 'supplier-row',
+    selected: false,
+
+    initialize: function() {
+      SupplierRowView.__super__.initialize(this, arguments);
+      this.delegate('click', '.checkbox input[type=checkbox]', this.checked);
+      this.delegate('click', 'td', this.open);
+    },
+
+    checked: function(evt) {
+      this.selected = $(evt.target).is(':checked');
+      this.$el.toggleClass('selected', this.selected);
+      this.trigger('selected');
+    },
+
+    open: function(evt) {
+      if ($(evt.target).is('.checkbox input[type=checkbox]')) return this.checked(evt);
+      console.log("open:", this.model);
+    }
 
   });
 
