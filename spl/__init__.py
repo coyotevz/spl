9bf31786d2c86068354cad1b3a4b6ab4f7c82cb2
Augusto Roccasalva <augusto@rocctech.com.ar>
2013-12-04 20:22:41 -0300

    Revert "Save unordered changes: to total disaster"
    This reverts commit 5d60f1f850c4a021e9eac081625d078fa69ee9a0.

--- spl/__init__.py
+++ spl/__init__.py
@@ -49,12 +49,6 @@ def configure_app(app, config=None):
     def dev_design():
         from flask import render_template
         from spl.models import db
-        return render_template('design.html', db=db)
-
-    @app.route('/design2/')
-    def dev_design():
-        from flask import render_template
-        from spl.models import db
         return render_template('design2.html', db=db)
 
     @app.before_request
